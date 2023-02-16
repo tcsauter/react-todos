d94c77d9069a41282a0200c2526b85b024d99af9
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-02-16 08:03:00 -0500

    Add Delete Todos function. Add ability to show and hide new Todo form.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
 
+    const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
     const [todos, setTodos] = useState(
         [
             {rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "User One"},
@@ -15,14 +17,25 @@ function App() {
     );
 
     const addTodo = (description, assigned) => {
+        let rowNumber;
         if (todos.length > 0) {
+            rowNumber = todos[todos.length - 1].rowNumber + 1
+        }else{
+            rowNumber = 1;
+        }
             const newTodo = {
-                rowNumber: todos.length + 1,
+                rowNumber: rowNumber,
                 rowDescription: description,
                 rowAssigned: assigned
             };
             setTodos(todos => [...todos, newTodo]);
-        }
+    }
+
+    const deleteTodo = (deleteTodoRowNumber) => {
+        let filtered = todos.filter(function (value) {
+            return value.rowNumber !== deleteTodoRowNumber;
+        })
+        setTodos(filtered);
     }
 
   return (
@@ -32,11 +45,13 @@ function App() {
                 Your Todos
             </div>
             <div className='card-body'>
-                <TodoTable todos={todos} />
-                {/*<button className="btn btn-primary" onClick={addTodo}>*/}
-                {/*    Add new todo*/}
-                {/*</button>*/}
-                <NewTodoForm addTodo={addTodo}/>
+                <TodoTable todos={todos} deleteTodo={deleteTodo} />
+                <button className="btn btn-primary" onClick={() => setShowAddTodoForm(!showAddTodoForm)}>
+                    {showAddTodoForm ? 'Close New Todo' : 'New Todo'}
+                </button>
+                {showAddTodoForm &&
+                    <NewTodoForm addTodo={addTodo} />
+                }
             </div>
         </div>
     </div>
